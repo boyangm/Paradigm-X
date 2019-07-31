3472454764d25aa260a36053620dd471ab8e35c7
Boyang Matsapola <boyangbass@gmail.com>
2019-07-30 21:54:04 -0500

    started the logo array

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,12 @@ window.onload = function (){
     const teamsList = $('#teamsList');
     const playerList = $('#playerList');
     function logo(team){
-        let teamLogo = [{team: "Chicago Bulls", source: 'https://teamwork-online-production.s3.amazonaws.com/uploads/e76d6979-d3ca-4c74-9a20-ee56afeffb4b.png'}]
+        let teamLogo = [{team: "Chicago Bulls", source: 'https://teamwork-online-production.s3.amazonaws.com/uploads/e76d6979-d3ca-4c74-9a20-ee56afeffb4b.png'},{team: "Atlanta Hawks", source: 'https://teamwork-online-production.s3.amazonaws.com/uploads/5bf18a88-84fb-49a2-843d-098dd7ff5425.png'},
+        {team: "Boston Celtics", source: 'https://secure.img1-fg.wfcdn.com/im/63371552/resize-h600%5Ecompr-r85/2901/29013525/NBA+Boston+Celtics+Roundel+27+in.+x+27+in.+Non-Slip+Indoor+Only+Mat.jpg'},{team: "Brooklyn Nets", source: 'https://fanatics.frgimages.com/FFImage/thumb.aspx?i=/productimages/_3336000/altimages/ff_3336044-3af6a38d747356b103c7alt1_full.jpg&w=900'},
+        {team: "Charlotte Hornets", source: 'https://store.nba.com/charlotte-hornets/charlotte-hornets-fathead-logo-giant-removable-decal/t-36817344+p-1447701665617+z-9-3782767136'},{team: "Cleveland Cavaliers", source: 'https://cdn.shopify.com/s/files/1/0993/8094/products/Cleveland_Cavaliers_3D_Fan_Foam_Logo_Sign_HD_Pro_Image_Sports_1024x1024.jpg?v=1504892006'},
+    
+    
+    ]
        teamLogo.map( name => {
            if (name.team === team){
               $('#logoArea').css('background',`url(${name.source}) center no-repeat`);
